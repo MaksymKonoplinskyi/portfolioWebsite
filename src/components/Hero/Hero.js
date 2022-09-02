a7b41b4673357b5fcac65bc4237f3a985c9b4e38
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-09-02 20:21:17 +0300

    1

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+// import CV from '../../assets/cv.pdf';
+
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Hello, I'm <br />
+          Maksym Konoplinskyi
+        </SectionTitle>
+        <SectionText>
+        Fullstack Developer
+        </SectionText>
+        <Button onClick={props.handleClick}><a href='../../assets/cv.pdf' download>Download CV</a></Button>
+        
+      </LeftSection>
+    </Section>
+  </>
+);
+
+export default Hero;
---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Multi-User Blog',
+    description: "Full Stack MERN Application. A fully working APP is a multi-user blog for the Gran Extreme Club. Users can register, create their own posts and comment on posts of other users to discuss important events and current news related to the activities of the Gran Extreme Club.",
+      image: '/images/1.png',
+      tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://github.com/MaksymKonoplinskyi/blog',
+    visit: 'https://blog-frontend-ecru.vercel.app/',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce',
+    description:"Discover Amazing places in Japan. Travel company landing page for selling tours to Japan. ",
+    image: '/images/2.png',
+    tags: ['HTML5', 'CSS', 'JavaScript'],
+    source: 'https://github.com/MaksymKonoplinskyi/lending7',
+    visit: 'https://maksymkonoplinskyi.github.io/lending7/',
+    id: 1,
+  },
+  {
+    title: 'WebRTC App',
+    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+      image: '/images/3.jpg',
+      tags: ['React', 'WebRTC'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Unichat',
+    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: '/images/4.jpg',
+    tags: ['React', 'ChatEngine', 'Firebase'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
