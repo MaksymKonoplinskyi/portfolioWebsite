77c685244da8f130f0f627d23c8cd5e06503d9a0
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-09-03 13:56:37 +0300

    2

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,9 +38,10 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
+  { year: 2005, text: 'Founded Extrim Club Gran', },
+  { year: 2016, text: 'Received a Ph.D.', },
   { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2019, text: 'Started my journey', },
   { year: 2020, text: 'Shared my projects with the world', },
   { year: 2021, text: 'Started my own platform', },
 ];
