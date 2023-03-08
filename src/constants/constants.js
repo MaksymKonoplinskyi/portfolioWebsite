803173ded1b8ced06cd8fc6786c124e79da73f48
MaksymKonoplinskyi <gran7777777@gmail.com>
2023-03-08 15:31:16 +0200

    f

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,72 +1,84 @@
 export const projects = [
   {
-    title: 'Multi-User Blog',
-    description: "Full stack MERN application. A fully working APP is a multi-user blog for the Gran Extreme Club. Users can register, create their own posts and comment on posts of other users to discuss important events and current news related to the activities of the Gran Extreme Club.",
-    image: '/images/1.png',
-    tags: ['Mongo', 'Express', 'React', 'Node', 'SASS'],
-    source: 'https://github.com/MaksymKonoplinskyi/blog',
-    visit: 'https://gran-blog.vercel.app/',
+    title: "Multi-User Blog",
+    description:
+      "Full stack MERN application. A fully working APP is a multi-user blog for the Gran Extreme Club. Users can register, create their own posts and comment on posts of other users to discuss important events and current news related to the activities of the Gran Extreme Club.",
+    image: "/images/1.png",
+    tags: ["Mongo", "Express", "React", "Node", "SASS"],
+    source: "https://github.com/MaksymKonoplinskyi/blog",
+    visit: "https://gran-blog.vercel.app/",
     id: 0,
-  },  
+  },
   {
-    title: 'E-Commerce',
-    description: "A fully working marketplace. The application is built on NEXT.JS using the React framework. Sanity is used as an СMS system. It has a fully responsive design and can be quickly re-targeted to any products. Maeketplace has integrated international payment system Stripe.",
-    image: '/images/4.png',
-    tags: ['Sanity', 'React', 'Next JS', 'JavaScript', 'Stripe'],
-    source: 'https://github.com/MaksymKonoplinskyi/shop',
-    visit: 'https://shop-mk.vercel.app/',
+    title: "E-Commerce",
+    description:
+      "A fully working marketplace. The application is built on NEXT.JS using the React framework. Sanity is used as an СMS system. It has a fully responsive design and can be quickly re-targeted to any products. Maeketplace has integrated international payment system Stripe.",
+    image: "/images/4.png",
+    tags: ["Sanity", "React", "Next JS", "JavaScript", "Stripe"],
+    source: "https://github.com/MaksymKonoplinskyi/shop",
+    visit: "https://shop-mk.vercel.app/",
     id: 3,
   },
   {
-    title: 'Uniswap clone',
-    description: "Fully working decentralized Web3 application. The DAPP uses smart contracts created and deployed using crunq technology as a backend. Deploy smart contracts on the Goerli test network. Quick migration to the Ethereum mainnet or another EVM compatible network is possible.",
-    image: '/images/3.png',
-    tags: ['Cranq', 'React', 'Tailwind', 'Alchemy', 'Solidity'],
-    source: 'https://github.com/MaksymKonoplinskyi/uniswap3.0',
-    visit: 'https://uniswap3.vercel.app/',
+    title: "Uniswap clone",
+    description:
+      "Fully working decentralized Web3 application. The DAPP uses smart contracts created and deployed using crunq technology as a backend. Deploy smart contracts on the Goerli test network. Quick migration to the Ethereum mainnet or another EVM compatible network is possible.",
+    image: "/images/3.png",
+    tags: ["Cranq", "React", "Tailwind", "Alchemy", "Solidity"],
+    source: "https://github.com/MaksymKonoplinskyi/uniswap3.0",
+    visit: "https://uniswap3.vercel.app/",
     id: 1,
   },
   {
-    title: 'Lending page',
-    description: "Discover Amazing places in Japan. Travel company landing page for selling tours to Japan. The main steps to optimize the site for search engines are quite complete: semantically correct HTML5 is used, the code is checked for validity. Site loading speed has been optimized.",
-    image: '/images/2.png',
-    tags: ['HTML5', 'CSS', 'JavaScript'],
-    source: 'https://github.com/MaksymKonoplinskyi/lending7',
-    visit: 'https://maksymkonoplinskyi.github.io/lending7/',
+    title: "Lending page",
+    description:
+      "Discover Amazing places in Japan. Travel company landing page for selling tours to Japan. The main steps to optimize the site for search engines are quite complete: semantically correct HTML5 is used, the code is checked for validity. Site loading speed has been optimized.",
+    image: "/images/2.png",
+    tags: ["HTML5", "CSS", "JavaScript"],
+    source: "https://github.com/MaksymKonoplinskyi/lending7",
+    visit: "https://maksymkonoplinskyi.github.io/lending7/",
     id: 2,
   },
-
+  {
+    title: "AI imege generator",
+    description:
+      "Create images through DALL-E AI and share them with the community. The full stack MERN application allows you to generate images using AI through the OpenAI API. The result can be shared with the community using Cloudinary. Implemented live search and download capability.",
+    image: "/images/5.png",
+    tags: ["Java Script", "React", "Node JS", "Express", "Tailwind"],
+    source: "https://github.com/MaksymKonoplinskyi/dall-e_v2",
+    visit: "https://dall-e-v2.vercel.app/",
+    id: 5,
+  },
 ];
 
 export const technologies = [
   {
-    title: 'Front-End',
-    tags: ['HTML 5', 'Java Script', 'React', 'Redux'],
+    title: "Front-End",
+    tags: ["HTML 5", "Java Script", "React", "Redux"],
     id: 0,
   },
   {
-    title: 'Styles',
-    tags: ['CSS 3', 'SASS', 'Tailwind', 'Material-UI'],
-    id: 0,
-  },
-  {
-    title: 'Back-End',
-    tags: ['Node JS', 'Express', 'Sequelize', 'MongoDB'],
+    title: "Styles",
+    tags: ["CSS 3", "SASS", "Tailwind", "Material-UI"],
     id: 1,
   },
   {
-    title: 'Web 3',
-    tags: ['Solidity', 'Cranq', 'Hardhat'],
-    id: 1,
+    title: "Back-End",
+    tags: ["Node JS", "Express", "Sequelize", "MongoDB"],
+    id: 2,
   },
-
+  {
+    title: "Web 3",
+    tags: ["Solidity", "Cranq", "Hardhat"],
+    id: 3,
+  },
 ];
 
 export const TimeLineData = [
-  { year: 2006, text: 'Founded Extrim Club Gran', },
-  { year: 2016, text: 'Received a Ph.D.', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Started my journey', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2006, text: "Founded Extrim Club Gran" },
+  { year: 2016, text: "Received a Ph.D." },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Started my journey" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
 ];
